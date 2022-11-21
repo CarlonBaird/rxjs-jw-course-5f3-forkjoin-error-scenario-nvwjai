ab8fda0efac37b54e351830a9661237503cf29ed
CarlonBaird <carlyestrella@live.com>
2022-11-21 14:09:51 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -5,12 +5,22 @@ const a$ = new Observable((subscriber) => {
     subscriber.next('A');
     subscriber.complete();
   }, 3000);
+
+  //Teardown logic
+  return () => {
+    console.log('Teardown 1');
+  };
 });
 
 const b$ = new Observable((subscriber) => {
   setTimeout(() => {
     subscriber.error('Failure');
   }, 3000);
+
+  //Teardown logic
+  return () => {
+    console.log('Teardown 2');
+  };
 });
 
 forkJoin([a$, b$]).subscribe({
