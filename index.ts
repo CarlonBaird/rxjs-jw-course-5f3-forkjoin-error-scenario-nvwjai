73e9d680d886a48dcf7baa59d3865ec92cc9a8b8
CarlonBaird <carlyestrella@live.com>
2022-11-21 13:56:04 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
+import { forkJoin, Observable } from 'rxjs';
 
+const a$ = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.next('A');
+    subscriber.complete();
+  }, 3000);
+});
 
+const b$ = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.error('Failure');
+  }, 3000);
+});
+
+forkJoin([a$, b$]);
